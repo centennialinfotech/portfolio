c24e3c303bfccaa92e37d8a6e986be7bf8745c8f
abbascentennia <mabbas.centennialinfotech@gmail.com>
2026-07-08 23:52:38 +0530

    Update PublicLayout.jsx

--- frontend/src/layouts/PublicLayout.jsx
+++ frontend/src/layouts/PublicLayout.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+
 export default function PublicLayout({ children }) {
   return (
     <div className="min-h-screen bg-black text-white">
-      <Navbar />
-      {children}
+      <div className="fixed top-0 left-0 right-0 z-50 bg-black">
+        <Navbar />
+      </div>
+      {/* Add padding so content doesn't hide behind navbar */}
+      <div className="pt-20">{children}</div>
     </div>
   );
 }
